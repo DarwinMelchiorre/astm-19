1d8658b4f5dfa480bf20eae3216539c5f51bc8ef
Darwin Melchiorre <146883269+DarwinMelchiorre@users.noreply.github.com>
2023-10-03 14:04:25 -0700

    Add files via upload

---
+++ Journal1.py
@@ -0,0 +1,16 @@
+#This program will give a few sentences to introduce myself
+
+#This function will tell the computer what to print
+def PrintIntroToMe():
+	print('Hi my name is Darwin Melchiorre (she/her).')
+	print('Having seen all the directors cuts and many many hours of behind the scenes my favorite movies are the Lord of the Ring trilogy.')
+	print('My favorite food is sushi, I think sharing it is the best so you can try lots of different things.')
+
+#This defines the main() function for this program
+def main():
+	PrintIntroToMe()
+
+#When we run the program
+#this executes first
+if __name__ == '__main__':
+	main()
