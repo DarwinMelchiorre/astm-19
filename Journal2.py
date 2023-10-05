bf938c533a17eb9205f01fbc2ef64e75171ab8da
Darwin Melchiorre <146883269+DarwinMelchiorre@users.noreply.github.com>
2023-10-05 15:00:22 -0700

    Add files via upload

---
+++ Journal2.py
@@ -0,0 +1,27 @@
+#Write a Python that prints 
+#the sum of two floating point numbers,
+# the difference between two integers, and 
+#the product of a floating point number and an integer. 
+#In each case, have the program print out 
+#the data type of the resulting answer.
+
+#Print the sum of two floating point numbers and data type
+a = 2.
+b = 5.4
+p = a+b 
+print(p)
+print(type(p))
+
+#Print the difference between two integers and data type
+a = 2
+b = 3
+p = b-a
+print(p)
+print(type(p))
+
+#Print the product of a floating point number and an integer and data type
+a = 3
+b = 3.4
+p = a*b
+print(p)
+print(type(p))
